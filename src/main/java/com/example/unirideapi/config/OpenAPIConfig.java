bdb78cfa160348a9f4d3b2a72efaccf4c03318b8
NicoleBances <nicolebances929@gmail.com>
2025-11-03 23:30:55 -0500

    Se realizaron las configuraciones necesarias para utilizar Swagger

---
+++ src/main/java/com/example/unirideapi/config/OpenAPIConfig.java
@@ -0,0 +1,45 @@
+package com.example.unirideapi.config;
+
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Contact;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.servers.Server;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import java.util.List;
+
+@Configuration
+public class OpenAPIConfig {
+
+    @Value("${cashflow.openapi.dev-url}")
+    private String devUrl;
+
+    @Value("${cashflow.openapi.prod-url}")
+    private String prodUrl;
+
+    @Bean
+    public OpenAPI myOpenAPI(){
+        Server devServer = new Server();
+        devServer.setUrl(devUrl);
+        devServer.setDescription("Server URL in Development environment");
+
+        Server prodServer = new Server();
+        prodServer.setUrl(prodUrl);
+        prodServer.setDescription("Server URL in Production environment");
+
+        Contact contact = new Contact();
+        contact.setEmail("bances929@email.com");
+        contact.setName("Nicole");
+        contact.setUrl("https://github.com/NicoleBances");
+
+        Info info = new Info()
+                .title("UniRide API")
+                .version("1.0")
+                .contact(contact)
+                .description("UniRide API description");
+
+        return new OpenAPI().info(info).servers(List.of(devServer, prodServer));
+    }
+}
